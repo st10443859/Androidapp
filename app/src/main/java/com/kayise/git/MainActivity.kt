50c7cf481dc8ab02a08ad3c67b4b7480847004ae
lab_services_student <st10443859@vcconnect.edu.za>
2024-03-18 09:53:47 +0200

    my first android app

---
+++ app/src/main/java/com/kayise/git/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kayise.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //declare a variable for birthdate and initialize
+    private val birthdate = 15
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
